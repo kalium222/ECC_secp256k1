7788a16ca73f0eb17871ebfc355c2c014d2e5e84
kalium222 <kalium222@sjtu.edu.cn>
2023-07-16 01:17:09 +0800

    feat: add Cipolla algorithm

--- BigInt.hpp
+++ BigInt.hpp
@@ -11,6 +11,40 @@ class BigInt {
 private:
     mpz_t value;
 
+    // Tool functions to compute quadratic residue
+    
+    // treat w like imaginary unit
+    static void extend_mul(
+        BigInt& resultx, BigInt& resulty,
+        const BigInt& x1, const BigInt& y1,
+        const BigInt& x2, const BigInt& y2,
+        const BigInt& w_square, const BigInt& p
+    ) {
+        resultx = (x1*x2 + y1*y2*w_square) % p;
+        if (resultx < 0) resultx += p;
+        resulty = (x1*y2 + x2*y1) % p;
+        if (resulty < 0) resulty += p;
+    }
+
+    // use binary method to compute a^n mod p
+    static void extend_pow(
+        BigInt& resultx, BigInt& resulty,
+        const BigInt& x, const BigInt& y,
+        const BigInt& n, const BigInt& w_square, const BigInt& p
+    ) {
+        resultx = 1;
+        resulty = 0;
+        BigInt exp = n;
+        BigInt tmpx = x, tmpy = y;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                extend_mul(resultx, resulty, resultx, resulty, tmpx, tmpy, w_square, p);
+            }
+            extend_mul(tmpx, tmpy, tmpx, tmpy, tmpx, tmpy, w_square, p);
+            exp /= 2;
+        }
+    }
+
 public:
     BigInt() {
         mpz_init2(value, NumBits);
@@ -199,6 +233,40 @@ public:
         mpz_urandomb(number, state, NumBits);
 
         return BigInt(number);
+    }
+
+    int getLegendraSymbol(const BigInt& p) const {
+        return mpz_legendre(this->value, p.value);
+    }
+
+    // For x^2 = n(*this) (mod p), return x
+    // There are 2 solutions, return the smaller one
+    BigInt getDiscreteSquareRootMod(const BigInt& p) const {
+        // if n = 0, then x = 0
+        if (this->value == 0) return BigInt(0);
+        // Check if n is a quadratic residue mod p
+        // by legendra symbol
+        if (this->getLegendraSymbol(p) != 1) {
+            // Very unlikely to happen
+            throw std::runtime_error("n is not a quadratic residue mod p");
+            return BigInt(-1);
+        }
+        // Randomly find a that a^2 - n is not a quadratic residue mod p
+        // This is easy because there are only (p-1)/2 possible values in this Group
+        // extended element w has w^2 = a^2 - n (mod p)
+        BigInt a, w_squqre;
+        while (true) {
+            a = BigInt::Random() % p;
+            w_squqre = (a * a - *this) % p;
+            if (w_squqre.getLegendraSymbol(p) == -1) break;
+        }
+        // Calculate x = (a + w)^((p+1)/2) (mod p)
+        // Like complex
+        BigInt x = a;
+        BigInt y = BigInt(1);
+        extend_pow(x, y, x, y, (p + 1) / 2, w_squqre, p);
+        x = (x%p + p) % p;
+        return (x < p - x) ? x : p - x;
     }
 };
 
